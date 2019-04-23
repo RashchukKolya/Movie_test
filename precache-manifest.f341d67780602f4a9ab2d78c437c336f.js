3c1278b65286ee78d9687cc3621dd06541d463bf
Nick <raschyk1993@gmail.com>
2019-04-23 12:10:41 +0300

    Updates

---
+++ precache-manifest.f341d67780602f4a9ab2d78c437c336f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff29c2d797c68c9c319",
+    "url": "/Movie_test/static/js/3.6d4f58b9.chunk.js"
+  },
+  {
+    "revision": "7cbeb0d9676bf08167fa",
+    "url": "/Movie_test/static/css/main.d96f47a4.chunk.css"
+  },
+  {
+    "revision": "6e197c81267ce8d0fee7",
+    "url": "/Movie_test/static/js/runtime~main.6738d345.js"
+  },
+  {
+    "revision": "f40fa6fecd3b3457031d",
+    "url": "/Movie_test/static/js/2.35dc9f40.chunk.js"
+  },
+  {
+    "revision": "8f27d0c0d61080eb29bb",
+    "url": "/Movie_test/static/js/5.5821ff16.chunk.js"
+  },
+  {
+    "revision": "7cbeb0d9676bf08167fa",
+    "url": "/Movie_test/static/js/main.3e51819d.chunk.js"
+  },
+  {
+    "revision": "203d9b76c19614c16659",
+    "url": "/Movie_test/static/js/4.4cf055a5.chunk.js"
+  },
+  {
+    "revision": "8f27d0c0d61080eb29bb",
+    "url": "/Movie_test/static/css/5.41fd320d.chunk.css"
+  },
+  {
+    "revision": "203d9b76c19614c16659",
+    "url": "/Movie_test/static/css/4.dadfa8ed.chunk.css"
+  },
+  {
+    "revision": "4ff29c2d797c68c9c319",
+    "url": "/Movie_test/static/css/3.1734b580.chunk.css"
+  },
+  {
+    "revision": "d0aae9de192c373c8187dc45d296d0dc",
+    "url": "/Movie_test/index.html"
+  }
+];
