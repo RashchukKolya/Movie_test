6af368ec893df0681df46437930ba39643890403
Nick <raschyk1993@gmail.com>
2019-04-23 12:26:58 +0300

    Updates

---
+++ precache-manifest.cf7d89b8040a7b4607c4c6af3c00dbc5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff29c2d797c68c9c319",
+    "url": "/Movie_test/static/js/3.6d4f58b9.chunk.js"
+  },
+  {
+    "revision": "ad07f6cd5c43ee3758de",
+    "url": "/Movie_test/static/css/main.d96f47a4.chunk.css"
+  },
+  {
+    "revision": "3adff4920dba69031251",
+    "url": "/Movie_test/static/js/runtime~main.2fc11384.js"
+  },
+  {
+    "revision": "f40fa6fecd3b3457031d",
+    "url": "/Movie_test/static/js/2.35dc9f40.chunk.js"
+  },
+  {
+    "revision": "7422f7a06997d5ff82ae",
+    "url": "/Movie_test/static/js/5.f37f1348.chunk.js"
+  },
+  {
+    "revision": "ad07f6cd5c43ee3758de",
+    "url": "/Movie_test/static/js/main.e6b0fb71.chunk.js"
+  },
+  {
+    "revision": "145191ed8d5ec4a7d87a",
+    "url": "/Movie_test/static/js/4.effea705.chunk.js"
+  },
+  {
+    "revision": "7422f7a06997d5ff82ae",
+    "url": "/Movie_test/static/css/5.41fd320d.chunk.css"
+  },
+  {
+    "revision": "145191ed8d5ec4a7d87a",
+    "url": "/Movie_test/static/css/4.dadfa8ed.chunk.css"
+  },
+  {
+    "revision": "4ff29c2d797c68c9c319",
+    "url": "/Movie_test/static/css/3.1734b580.chunk.css"
+  },
+  {
+    "revision": "02b32e4d1859a050870f743839ccce45",
+    "url": "/Movie_test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f3e1a6251dd365772c722e1215c0b2a.js"
+  "/Movie_test/precache-manifest.cf7d89b8040a7b4607c4c6af3c00dbc5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Movie_test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
